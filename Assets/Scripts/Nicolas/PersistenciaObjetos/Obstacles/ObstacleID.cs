f01a64360f5a20121812f12e076e9ae5574f3a23
Kenneth David Forero Laverde <135041748+KennethForero@users.noreply.github.com>
2025-06-16 09:17:58 -0500

    Parece Bien todo

--- Assets/Scripts/Nicolas/PersistenciaObjetos/Obstacles/ObstacleID.cs
+++ Assets/Scripts/Nicolas/PersistenciaObjetos/Obstacles/ObstacleID.cs
@@ -1,42 +1,14 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
-[ExecuteAlways]
 public class ObstacleID : MonoBehaviour
 {
-    [SerializeField]
     public string obstacleID;
 
-    [SerializeField] public string ObstacleIDValue => obstacleID;
-
-    private void Awake()
+    void Start()
     {
-            string sceneName = gameObject.scene.name;
-            string objectName = gameObject.name;
-            Vector3 position = transform.position;
-            // Generar ID legible: escena_nombre_posX_posY
-            obstacleID = $"{sceneName}_{objectName}_{Mathf.RoundToInt(position.x)}_{Mathf.RoundToInt(position.y)}";
-            // Marcar el objeto como modificado para que se guarde
-            EditorUtility.SetDirty(this);
+        if (ObstacleDestructionTracker.Instance.IsObstacleDestroyed(obstacleID))
+        {
+            gameObject.SetActive(false); // o Destroy(gameObject);
+        }
     }
-
-    // private void Start()
-    // {
-    //     if (string.IsNullOrEmpty(obstacleID))
-    //     {
-    //         string sceneName = gameObject.scene.name;
-    //         string objectName = gameObject.name;
-    //         Vector3 position = transform.position;
-
-    //         // Generar ID legible: escena_nombre_posX_posY
-    //         obstacleID = $"{sceneName}_{objectName}_{Mathf.RoundToInt(position.x)}_{Mathf.RoundToInt(position.y)}";
-
-    //         // Marcar el objeto como modificado para que se guarde
-    //         EditorUtility.SetDirty(this);
-    //     }
-    // }
 }
